b854665177b3abd0c0bc6703c2f088c4baa06068
Sarabjeet Singh <sabbispeak@gmail.com>
2018-02-25 22:46:01 +0530

    sample project changes

--- app/src/main/java/com/savvi/rangepickersample/SampleActivity.java
+++ app/src/main/java/com/savvi/rangepickersample/SampleActivity.java
@@ -3,6 +3,8 @@ package com.savvi.rangepickersample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.savvi.rangedatepicker.*;
 
@@ -12,11 +14,13 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class SampleActivity extends AppCompatActivity {
 
 
     CalendarPickerView calendar;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,38 +34,36 @@ public class SampleActivity extends AppCompatActivity {
         lastYear.add(Calendar.YEAR, -10);
 
         calendar = (CalendarPickerView) findViewById(R.id.calendar_view);
+        button = (Button) findViewById(R.id.get_selected_dates);
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
 
-        calendar.init(lastYear.getTime(), nextYear.getTime()) //
-                .inMode(CalendarPickerView.SelectionMode.RANGE) //
-                .withSelectedDate(new Date());
-
-
-
-        SimpleDateFormat dateformat = new SimpleDateFormat("dd-MM-yyyy");
-        String strdate = "22-06-2017";
-        String strdate2 = "26-06-2017";
-
-
-
-
+        calendar.deactivateDates(list);
+        ArrayList<Date> arrayList = new ArrayList<>();
         try {
+            SimpleDateFormat dateformat = new SimpleDateFormat("dd-MM-yyyy");
+            String strdate = "22-2-2018";
+            String strdate2 = "26-2-2018";
             Date newdate = dateformat.parse(strdate);
             Date newdate2 = dateformat.parse(strdate2);
-            ArrayList<Date> arrayList = new ArrayList<>();
             arrayList.add(newdate);
             arrayList.add(newdate2);
-            calendar.highlightDates(arrayList);
         } catch (ParseException e) {
             e.printStackTrace();
         }
+        calendar.init(lastYear.getTime(), nextYear.getTime(), new SimpleDateFormat("MMMM, YYYY", Locale.getDefault())) //
+                .inMode(CalendarPickerView.SelectionMode.RANGE) //
+                .withSelectedDate(new Date())
+                .withDeactivateDates(list)
+                .withHighlightedDates(arrayList);
 
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(7);
 
-        calendar.deactivateDates(list);
-
-        Log.d("list",  calendar.getSelectedDates().toString());
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("list",  calendar.getSelectedDates().toString());
+            }
+        });
 
 
     }
